642029a87c7e217cfafc5f4904ebd237d298798a
lukasg18 <lukas.gomes2010@gmail.com>
2019-05-29 10:36:09 -0300

    heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/poo2-frontend'));
+app.use(express.static(__dirname + '/dist/locadora-frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/poo2-frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/locadora-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
